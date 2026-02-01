19bb8690671712fb82b3697ce8b14dc4d460d419
Alexandra Lara <alexandrailp.r@gmail.com>
2026-02-01 00:25:54 -0300

    Se finaliza estructura base

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
-precio = 400000
+const precio = 400000;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+const precioSpan = document.querySelector(".precio-inicial");
+const cantidadSpan = document.querySelector(".cantidad");
+const totalSpan = document.querySelector(".valor-total");
+
+let cantidad = 0;
+
+precioSpan.innerHTML = precio;
+cantidadSpan.innerHTML = cantidad;
+totalSpan.innerHTML = 0;
+
